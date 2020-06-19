4a83d19e2d2e8f523f35de80f040af9291f4e0cf
Svetsyn <svetsyn@gmail.com>
2020-06-19 13:58:56 +0300

    Added new task-Car_Shop

---
+++ OOP/Exercise/Interfaces_And_Abstraction/Car_Shop/Car.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    Integer TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+}
